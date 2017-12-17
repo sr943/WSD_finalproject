a12ae4b5acb36e65ad363560e9a5cb3c9b28e5b0
shubhangi rakhe <sr943@afsconnect2.njit.edu>
2017-12-17 16:40:03 -0500

    eighteenth commit

--- mvc/pages/homepage.php
+++ mvc/pages/homepage.php
@@ -1,8 +1,4 @@
 
-
-
-
-
 <!doctype html>
 
 <html lang="en">
@@ -14,17 +10,17 @@
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
 </head>
 
 
-<body background="alltask.jpg">
+<body background="bgimage.jpg" style = "background-repeat:no-repeat; background-size:cover;">
 
- 
-<h1><center> <font color="#800000"><u>
+ <br>
+<h1><center> <font color="White" ><u>
     <?php
 
     //this how to print some data;
@@ -32,31 +28,63 @@
 
     ?></u></font></center> </h1>
 
-<!--<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1> 
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1> -->
 
-<form action="index.php?page=accounts&action=login" method="POST">
+
+<br>
 
     <div class="container">
-        <h2> Login</h2>
-        <div>
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
 
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+<form class="form-horizontal" action="index.php?page=accounts&action=login" method="POST" >    
+        
+         <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <h2>Welcome!!</h2>
+                    <hr>
+                </div>
+            </div>
+           
         
         
-            <button style="background-color:lightblue" type="submit">Login</button>
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+            <label class="sr-only" for="email"><b>Username</b></label>
+              <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                 <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
+                  <input type="text" class="form-control" placeholder="Enter Username" name="email" required autofocus>
+                </div>
+           </div>
         </div>
-    </div>
-
-
-</form>
-
-<h2><a href="index.php?page=accounts&action=register">Not a member yet?</a></h2>
+        
+        <br/>
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
+         
+           <label class="sr-only" for="password"><b>Password</b></label>
+            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+            <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
+            <input class="form-control" type="password"  placeholder="Enter Password" name="password" required>
+            </div>
+          </div>   
+        </div>
+                
+        
+        <div class="row" style="padding-top: 1rem">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+            
+            <button type="submit" class="btn btn-success"><i class="fa fa-sign-in"></i> Login</button>
+            <a href="index.php?page=accounts&action=register" class="btn btn-link" >Not a member yet?</a>
+       </div>
+       </div>
+        
+  </form>
+  </div>
+  </div>
 
 
 <script src="js/scripts.js"></script>
 </body>
-</html>
+</html> 
